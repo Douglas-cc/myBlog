646f0e3847a049f5ed13cd68835bf72229fc4278
Douglas-cc <douglasdasilvasousa8@gmail.com>
2022-11-23 17:10:22 -0300

    alterações database

--- database/database.js
+++ database/database.js
@@ -1,22 +1,24 @@
 const Sequelize = require('sequelize')
-const dotenv = require('dotenv')
+// const dotenv = require('dotenv')
 
-dotenv.config()
+// dotenv.config()
 
-database = process.env.MYSQL_DATABASE
-user = process.env.MYSQL_USER
-host = process.env.MYSQL_HOST
-port = process.env.MYSQL_LOCAL_PORT
-password = process.env.MYSQL_ROOT_PASSWORD
+// database = process.env.MYSQL_DATABASE
+// user = process.env.MYSQL_USER
+// host = process.env.MYSQL_HOST
+// port = process.env.MYSQL_LOCAL_PORT
+// password = process.env.MYSQL_ROOT_PASSWORD
 
 const connection = new Sequelize(
-  database, 
-  user,
-  password, {
-    host: host,
-    port: port,
+  'db-blog', 
+  'root',
+  'jedi1290', {
+    host: 'db',
+    port: 3306,
     dialect: 'mysql',
     timezone: '-03:00',
 })
+
+connection.sync({});
 
 module.exports = connection
